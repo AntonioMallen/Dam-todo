2ac863624dd13f716227c8d08c38e7f30d19d60e
AntonioMallen <aa21.amallen98@iespabloserrano.com>
2022-03-22 14:22:07 +0100

    a

--- eclipse workspace/Tema 9/src/SeleccionDirecta.java
+++ eclipse workspace/Tema 9/src/SeleccionDirecta.java
@@ -1,7 +1,7 @@
 
 public class SeleccionDirecta {
 	int [] vector= { 6, 17, 4, 7,99,13,22,66} ;
-	int cantidad= vector.length-1;
+	int cantidad= vector.length;
 	int aux=0;
 	
 	void seleccionDirecta() {
@@ -30,15 +30,103 @@ public class SeleccionDirecta {
 					menor=j;
 				}
 			}
+			if(i!=menor) {
+				aux=vector[i];	
+				for(int k=menor;k>i;k--)
+					vector[k]=vector[k-1];
+				vector[menor]=aux;
+			}
 			
-			aux=vector[i];
-			vector[menor]=vector[i];
-			vector[menor]=aux;
+		}
+	}
+	void seleccionDirecta_ionut() {
+		for(int i=0;i<cantidad;i++) {
+			int menor=i;
+			for(int j=i+1;j<cantidad;j++) {
+				if(vector[menor]>vector[j]) {
+					aux=vector[j];
+					vector[j]=vector[menor];
+					vector[menor]=aux;
+				}
+			}
+			
 		}
 	}
 	
+	void burbuja() {
+
+		for (int i = 0; i < cantidad-1; i++) {
+			
+			for(int j=0;j<cantidad-(i+1);j++) {
+				if(vector[j]>vector[j+1]) {
+					aux=vector[j];
+					vector[j]=vector[j+1];
+					vector[j+1]=aux;
+				}
+			} 
+			
+		}
+	}
+	/*
+	 * No sabemos si es esto lo que nos pide pero Antonio Mallén dice que sí☺
+	 */
+	void burbuja_pro() {
+		boolean ordenao=false;
+		do {
+			for (int i = 0; i < cantidad; i++) {
+				for(int j=0;j<cantidad-(i+2);j++) {
+					if(vector[j]>vector[j+1]) {
+						aux=vector[j];
+						vector[j]=vector[j+1];
+						vector[j+1]=aux;
+					}
+					if(vector[j+1]>vector[j+2]) {
+						aux=vector[j+1];
+						vector[j+1]=vector[j+2];
+						vector[j+2]=aux;
+					}else {
+						ordenao=true;
+					}
+				}
+				
+				
+			}
+		}while(!ordenao);
+	}
+	void burbuja_pro_bien() {
+		boolean ordenao=false;
+		int u=cantidad-1;
+		int p=0;
+		do {
+			for (int j = u; j>=p ;j--) {
+				if(vector[j]>vector[j+1]) {
+					if(vector[j]>vector[j+1]) {
+						aux=vector[j];
+						vector[j]=vector[j+1];
+						vector[j+1]=aux;
+					}
+				}	
+			}
+			p++;
+			for(int j=p;j<=u;j++) {
+				if(vector[j]<vector[j+1]) {
+					if(vector[j]>vector[j+1]) {
+						aux=vector[j];
+						vector[j]=vector[j+1];
+						vector[j+1]=aux;
+					}
+				}
+				u--;
+			}
+		}while(p<u);
+	}
+	
 	public static void main(String[] args) {
-		
+		SeleccionDirecta si = new SeleccionDirecta();
+		si.burbuja_pro();
+		for(int i=0;i<si.vector.length;i++) {
+			System.out.println(si.vector[i]);
+		}
 	}
 
 }
